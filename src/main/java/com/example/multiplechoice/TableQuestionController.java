2aba65505e4bb28ef887e809c584714d0906ff37
your name <truongdat.work@gmail.com>
2023-04-01 16:44:08 +0700

    update event add button in tableQuestion

--- src/main/java/com/example/multiplechoice/TableQuestionController.java
+++ src/main/java/com/example/multiplechoice/TableQuestionController.java
@@ -1,13 +1,20 @@
 package com.example.multiplechoice;
 
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -42,5 +49,14 @@ public class TableQuestionController implements Initializable {
         categoryQuestionColumn.setCellValueFactory(new PropertyValueFactory<Question, String>("categoryQuestion"));
         markQuestionColumn.setCellValueFactory(new PropertyValueFactory<Question, Integer>("markQuestion"));
         tableQuestion.setItems(questionList);
+    }
+    public void creatQuestionButton(ActionEvent e) throws IOException {
+        Stage stage = (Stage)((Node) e.getSource()).getScene().getWindow();
+        FXMLLoader AddQuestion = new FXMLLoader();
+        AddQuestion.setLocation(getClass().getResource("AddQuestion.fxml"));
+        Parent questionViewAdd = AddQuestion.load();
+        Scene sceneAddQuestion = new Scene(questionViewAdd);
+        stage.setScene(sceneAddQuestion);
+
     }
 }
